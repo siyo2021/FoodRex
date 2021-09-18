4357e5ed987ebbe5f7d372c0523838c709791ecb
Yousef Bulbulia <yousefbulbulia1@gmail.com>
2021-09-18 12:53:02 -0400

    Styled landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import RecPage from "./RecPage";
+import Landing from "./Landing.js";
 import Footer from "./components/Footer";
 import theme from "./theme.js";
 import "@fontsource/alegreya-sans";
@@ -9,7 +9,7 @@ import "@fontsource/alegreya-sans-sc";
 function App() {
 	return (
 		<ChakraProvider theme={theme}>
-			<RecPage />
+			<Landing />
 			<Footer />
 		</ChakraProvider>
 	);
--- frontend/src/Landing.js
+++ frontend/src/Landing.js
@@ -1,7 +1,17 @@
 
 //TODO: Add the logo and header (curve) images in the title div!
 
-import { FormControl, Input } from "@chakra-ui/react";
+import {
+    FormControl,
+    Input,
+    Button,
+    Text,
+    Image,
+    Heading,
+    Box,
+    HStack,
+    Center
+} from "@chakra-ui/react";
 
 import RecCard from "./components/RecCard";
 
@@ -9,31 +19,31 @@ function Landing() {
     return (
 
         <div>
+            <Box bg="orange" align-items="center">
+                <HStack>
+                    <Image src="../Dino.png" alt="A dinosaur" boxSize="80px" />
+                    <Heading>Food Rex</Heading>
+                </HStack>
+                <Text fontSize="md" color="white">Don’t know what to eat?  We got you!</Text>
+            </Box>
 
-            <RecCard />
-            <div className="title">
-                <!-- TODO: Add dino image -->
-                <h1>Food Rex</h1>
-                <h5>Don’t know what to eat?  We got you!</h5>
-                <!-- TODO: Add curved image -->
-            </div>
-            <div className="tacoSection">
-                <!-- TODO: Add taco image -->
-            </div>
+            <Box bg="gray.300">
+                <Image src="../Taco.jpg" alt="A taco" />
+            </Box>
 
-            <div className="postalCodeEntry">
-                <FormControl id="postalCode">
-                    <Input placeholder="Enter your postal code to get recommendations" />
-                </FormControl>
-            </div>
-
-            <div className="aboutUs">
-                <h3>About Us</h3>
-                <p>Food Rex (short for “food recommendations”) is a free, AI-powered recommendation engine that enables you to find your next meal with just a few swipes. Enter your postal code to see it in action!</p>
-            </div>
-
+            <Box bg="brownGray" padding="90px 0 75px" alignItems="center">
+                <form>
+                    <FormControl id="postalCode">
+                        <Input placeholder="Enter your postal code to get recommendations" width="45%" borderRadius="full" bg="white" mb="30px"/>
+                    </FormControl>
+                    <Button type="submit" bg="red" color="white" borderRadius="full" width="17%">Go!</Button>
+                </form>
+            </Box>
+            <Box>
+                <Text fontSize="xl">About Us</Text>
+                <Text>Food Rex (short for “food recommendations”) is a free, AI-powered recommendation engine that enables you to find your next meal with just a few swipes. Enter your postal code to see it in action!</Text>
+            </Box>
         </div>
-
 
     )
 }
