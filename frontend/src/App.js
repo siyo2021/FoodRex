e1f9187a0866cf815a01294e06c618014a8e8108
Niveditha Kani <nivedithakani@gmail.com>
2021-09-18 11:43:57 -0400

    Add recommendation page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import RecPage from "./RecPage";
 import theme from "./theme.js";
-import "@fontsource/raleway/400.css";
 import "@fontsource/alegreya-sans";
+import "@fontsource/alegreya-sans-sc";
 
 function App() {
 	return (
